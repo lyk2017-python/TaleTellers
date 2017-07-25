5438c069ddf98fd0778b1fedb72a49638024fa7b
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-07-25 21:16:13 +0300

    __str__ function added

--- taletellers/storyboard/models.py
+++ taletellers/storyboard/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, blank=True, null=True)
+    title = models.CharField(max_length=100, blank=True, null=True)         # unique=True might be added
     content = models.CharField(max_length=140, blank=True, null=True)
     creation_time = models.DateTimeField(auto_now_add=True)
     score = models.IntegerField(default=0)
     parent = models.ForeignKey("self", blank=True, null=True, related_name="children")
+
+    def __str__(self):
+        # returns the id and the title of the Post class's objects when it's requested on manage.py's shell
+        return "#{0} {1}".format(self.id, self.title)
