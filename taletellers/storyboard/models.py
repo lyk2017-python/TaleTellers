735149be4819508705e965dede668cdb9c0f5f7f
M. Bilal Kılıçoğlu <mbilalkilicoglu@gmail.com>
2017-07-25 19:47:15 +0300

    models.py created with Post class

--- taletellers/storyboard/models.py
+++ taletellers/storyboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    content = models.CharField(max_length=140, blank=True, null=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    score = models.IntegerField(default=0)
+    parent = models.ForeignKey("self", blank=True, null=True, related_name="children")
