f5a3af842386a60c09469f8b624e0f4f7379b980
M. Bilal Kılıçoğlu <mbilalkilicoglu@gmail.com>
2017-07-26 16:53:00 +0300

    Story names are linked to the stories

--- taletellers/storyboard/urls.py
+++ taletellers/storyboard/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from storyboard.views import *
 
 urlpatterns = [
-    url(r"^", HomeView.as_view(), name="home"),
+    url(r"^$", HomeView.as_view(), name="home"),
     url(r"^sss$", SSSView.as_view(), name="faq"),
-    url(r"^story/(?P<slug>[\A-Za-z0-9\-]+)$", StoryView.as_view(), name="story_detail"),
+    url(r"^story/(?P<pk>\d+)$", StoryView.as_view(), name="story_detail"),
     # url(r"^detay/(?P<id>\d+)-(?P<slug>[A-Za-z0-9\-]+)$", HaberView.as_view(), name="news_detail"),
 ]
