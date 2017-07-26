58f3bbba815c293b39a34fdfcbaffc8ff0e1c90d
Büşra Eryaşar <busranureryasar@gmail.com>
2017-07-26 17:19:12 +0300

    git conflict tests

--- taletellers/storyboard/urls.py
+++ taletellers/storyboard/urls.py
@@ -4,6 +4,6 @@ from storyboard.views import *
 urlpatterns = [
     url(r"^", HomeView.as_view(), name="home"),
     url(r"^sss$", SSSView.as_view(), name="faq"),
-    url(r"^story/(?P<slug>[\A-Za-z0-9\-]+)$", StoryView.as_view(), name="story_detail"),
+    url(r"^story/(?P<pk>\d+)$", StoryView.as_view(), name="story_detail"),
     # url(r"^detay/(?P<id>\d+)-(?P<slug>[A-Za-z0-9\-]+)$", HaberView.as_view(), name="news_detail"),
 ]
