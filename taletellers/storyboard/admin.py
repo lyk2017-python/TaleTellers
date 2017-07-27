cab571d5632907ccce3580ff0326625feba9239d
M. Bilal Kılıçoğlu <mbilalkilicoglu@gmail.com>
2017-07-27 12:15:45 +0300

    admin.py is created. Now admin panel for posts works

--- taletellers/storyboard/admin.py
+++ taletellers/storyboard/admin.py
@@ -4,4 +4,25 @@ from storyboard.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "title", "content", "creation_time", "parent"]
+    search_fields = ["title", "content"]
+    list_filter = ["creation_time", "title"]
+    fieldsets = [
+        (
+            "Globals", {
+                "fields": [
+                    "title",
+                    "content"
+                ]
+            }
+        ),
+        (
+            "Others", {
+                "fields": [
+                    "score",
+                    "parent"
+                ]
+            }
+        )
+
+    ]
