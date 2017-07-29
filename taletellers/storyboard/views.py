a0a724641cdc036e8a7fe34ca0560d6a0f38ff00
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-07-29 17:19:39 +0300

    fixed unnecessary override of get_form_kwargs method in AddStoryFromView class.

--- taletellers/storyboard/views.py
+++ taletellers/storyboard/views.py
@@ -41,13 +41,6 @@ class AddStoryFormView(generic.CreateView):
     def get_success_url(self):
         return reverse("add_story")
 
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        if self.request.method in ["POST", "PUT"]:
-            post_data = kwargs["data"].copy()
-            kwargs["data"] = post_data
-        return kwargs
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["post_list"] = list(Post.objects.filter(parent__isnull=True))
