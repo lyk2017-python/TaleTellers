1776fe697e6dd6a088719ac94fc50ff4fdc557d9
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-07-30 16:37:11 +0300

    code refactoring.

--- taletellers/storyboard/models.py
+++ taletellers/storyboard/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
