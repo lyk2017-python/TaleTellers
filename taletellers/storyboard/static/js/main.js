098d9007996f2d59c9d45050c3acd91130ae90ef
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-07-30 11:47:47 +0300

    jquery library added. ajax practices failed for now.

--- taletellers/storyboard/static/js/main.js
+++ taletellers/storyboard/static/js/main.js
@@ -1,8 +1,17 @@
-$('.navbar li').click(function(e) {
-    console.log("test");
-    $('.navbar li.active').removeClass('active');
-    var $this = $(this);
-    if (!$this.hasClass('active')) {
-        $this.addClass('active');
-    }
+$(document).ready(function() {
+
+    // up or down option with ajax is added
+
+    // $("#vote").submit(function(event) {
+    //     event.preventDefault();
+    //         $.ajax({
+    //            url: 'test_ajax',
+    //            data: 1,
+    //            // type: "POST",
+    //            success: function (data) {
+    //                console.log(data)
+    //            }
+    //         });
+    //         return false;
+    // });
 });
