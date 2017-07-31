1184d1d11873c6d42b25114f2bd656d95774a498
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-07-31 20:32:45 +0300

    example ajax codes for js added as comment to main.js.

--- taletellers/storyboard/static/js/main.js
+++ taletellers/storyboard/static/js/main.js
@@ -4,3 +4,18 @@ function show_details(content) {
 function hide_details(content) {
     content.style.backgroundColor = "white";
 }
+
+/*
+function getCookie(name) {
+	// django's official function, search => "django csrf ajax"
+}
+
+
+function inc_like(param) {
+	$.post({
+		data: "{'like': param, 'id': {{object.id}}, 'csrfmiddlewaretoken': getCookie('csrftoken')},
+		url: "{% url 'like_dislike' %}
+	});
+}
+
+ */
