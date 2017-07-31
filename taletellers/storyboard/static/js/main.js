f1926d8e6574546ea3e7e2a3d0596f82162bea32
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-07-31 20:33:59 +0300

    official django getCookie function added.

--- taletellers/storyboard/static/js/main.js
+++ taletellers/storyboard/static/js/main.js
@@ -7,8 +7,21 @@ function hide_details(content) {
 
 /*
 function getCookie(name) {
-	// django's official function, search => "django csrf ajax"
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
+var csrftoken = getCookie('csrftoken');
 
 
 function inc_like(param) {
